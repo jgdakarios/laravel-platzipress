56cd9e4d35bef18d40a2d3a4b01e961a519897a7
Joseph Dakkak <jgdakarios@gmail.com>
2020-08-18 10:56:58 -0400

    Creación de tablas, entidades y datos de prueba

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+
+        App\User::create([
+            'name'      => 'Joseph Dakkak',
+            'email'     => 'jgdakarios@gmail.com',
+            'password'  => bcrypt('testPassword')
+        ]);
+
+        factory( App\Post::class, 24)->create();
     }
 }
